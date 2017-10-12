ad0ca631b54ae25a5bf7f3749bb0956741a5114e
Renaud Forestié <reuno@reuno.net>
2017-10-12 13:03:51 +0200

    :sparkles: Adds ObjectActivatedZones

---
+++ ObjectActivatedZones/ObjectActivated.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+
+	/// <summary>
+	///***This script free to use and no credit is required. 
+	///***This is intended to be used with More Mountain's Corgi Engine 4.2+
+	///***Written by: Keith Henderson. Any questions can be sent to keith.donaldh@gmail.com
+	///***This script is to be used in conjunction with ObjectActivatedZone.cs
+	/// Add this script to a GameObject and you can call specific methods when all Object Activated Zones are activated. (Ex: Moving Platforms).
+	/// </summary>
+
+	public class ObjectActivated : MonoBehaviour {
+
+		[Header("Number of Zones Required to Activate")]
+		/// This is the number ObjectActivatedZones that are required to activate this object.
+		[Tooltip("Declare how many ObjectActivatedZones are required to activate this object.")]
+		public int RequiredZones = 1;
+
+		[Header("Method to Activate")]
+		/// The method that is called when all conditions are met
+		public UnityEvent EnableAction;
+
+		[Header("Method to Deactivate")]
+		/// The method that is called when all conditions are not met
+		public UnityEvent DisableAction;
+
+		//We use this to keep track of how many zones have been activated
+		[HideInInspector]
+		public int Counter;
+
+		public void ConditionCheck()
+		{
+			//We check to see if the counter matches the number of required zones. If not, we disable and exit
+		     if(Counter == RequiredZones)
+		     	Enable();
+
+		     else
+		    	Disable();
+		    	return;
+		}
+
+		/// Here we execute the method when all condtions are met
+		protected virtual void Enable()
+		{
+			if (EnableAction != null)
+			{
+				EnableAction.Invoke ();
+			}
+		}
+
+		///  Here we execute the method when all conditions are not met
+		protected virtual void Disable()
+		{
+			if (DisableAction != null)
+			{
+				DisableAction.Invoke ();
+			}
+		}
+
+
+	}
+}
---
+++ ObjectActivatedZones/ObjectActivatedZone.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+	[RequireComponent(typeof(Collider2D))]
+
+	/// <summary>
+	///***This script free to use and no credit is required. 
+	///***This is intended to be used with More Mountain's Corgi Engine 4.2+
+	///***Written by: Keith Henderson. Any questions can be sent to keith.donaldh@gmail.com
+	///***This script is to be used in conjunction with ObjectActivated.cs
+	/// Add this component to an object with a collider to created an Object Activated Zone
+	/// In the inspector you can specify which object is required to activate the zone with RequiredObject
+	/// You can also set the GameObject that will be activated when all the RequiredObjects are in the correct zones(This is the GameObject you add the ObjectActivated.cs component to).
+	/// </summary>
+
+	public class ObjectActivatedZone : MonoBehaviour {
+
+		[Header("Required Object")]
+		//This is the required object used to activate the zone, it must be set in inspector
+		[Tooltip("Place the GameObject that will activate the zone here.")]
+		public GameObject RequiredObject;
+
+		[Header("Activated Object")]
+		/// This is the object which is being activated when all conditions are met
+		[Tooltip("Place the GameObject that will be activated when all condtions are filled.")]
+		public GameObject ActivatedObject;
+
+		//Protected Stuff
+		protected Collider2D _collidingObject;
+		protected GameObject _activatedObject;
+
+
+		/// We initialize the ActivatedObject so we can update the Counter
+		void Awake (){
+
+			_activatedObject = ActivatedObject;
+		
+		}
+
+		// On enter we store our colliding object
+		protected virtual void OnTriggerEnter2D(Collider2D collider)
+			{
+				//If the player enters the collider we ignore it and exit
+				if (collider.gameObject.GetComponentNoAlloc<Character> ()) {
+					return;
+				} 
+
+				//We check to see if RequiredObject has been set and if the GameObject colliding is the RequiredObject
+				else {
+					_collidingObject = collider;
+					if (RequiredObject != null && _collidingObject.gameObject == RequiredObject)
+					{
+						//We make sure an ActivatedObject has been set in inspector, if not we do nothing
+						if(_activatedObject != null)
+						{
+							//We update the counter and check to see if conditions are met to activate object
+							_activatedObject.GetComponent<ObjectActivated>().Counter += 1;
+							_activatedObject.GetComponent<ObjectActivated>().ConditionCheck(); 	
+						}
+						else
+							Debug.Log("Please set the ActivatedObject in the inspector.");
+							return; 
+					}
+					else
+							Debug.Log("Either the RequiredObject has not been set, or it is the wrong object in the zone.");
+						return;
+				
+				}
+					
+			}
+
+		// On exit we store our colliding object
+		protected virtual void OnTriggerExit2D(Collider2D collider)
+			{
+				//If the player enters the collider we ignore it and exit
+				if (collider.gameObject.GetComponentNoAlloc<Character> ()) {
+					return;
+				} 
+
+				//We check to see if RequiredObject has been set and if the GameObject colliding is the RequiredObject
+				else {
+					_collidingObject = collider;
+					if (RequiredObject != null && _collidingObject.gameObject == RequiredObject)
+					{
+						//We make sure an ActivatedObject has been set in inspector, if not we do nothing
+						if(_activatedObject != null)
+						{
+							//We update the counter and check to see if conditions are met to activate object
+							_activatedObject.GetComponent<ObjectActivated>().Counter -= 1;
+							_activatedObject.GetComponent<ObjectActivated>().ConditionCheck(); 	
+						}
+						else
+							Debug.Log("Please set the ActivatedObject in the inspector.");
+							return; 
+					}
+					else
+							Debug.Log("Either the RequiredObject has not been set, or it is the wrong object in the zone.");
+						return;
+				
+				}
+					
+			}
+
+	}
+}
+
