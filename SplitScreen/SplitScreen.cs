50d87a7b181f3f2950871e0cc9dc8590ba0fe103
Renaud Forestie <renaud.forestie@gameloft.com>
2018-03-21 08:51:07 -0400

    :rocket: Updates the SplitScreen scripts

--- SplitScreen/SplitScreen.cs
+++ SplitScreen/SplitScreen.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
 
-namespace CameraUtils
-{
-    /// <summary>
-    /// Extends corgi's camera controller just to set the target
-    /// </summary>
-    public class SplitScreen : MoreMountains.CorgiEngine.CameraController
-    {
+namespace CameraUtils {
+  /// <summary>
+  /// Extends corgi's camera controller just to set the target
+  /// </summary>
+  public class SplitScreen : MoreMountains.CorgiEngine.CameraController {
 
-        /// <summary>
-        /// Set new target
-        /// </summary>
-        /// <param name="target"></param>
-        public void SetTarget(Transform target)
-        {
-            _target = target;
-        }
+    /// <summary>
+    /// Set new target for camera
+    /// </summary>
+    /// <param name="target"></param>
+    public void SetTarget(Transform target) {
+      _target = target;
+
+      if (_target.GetComponent<CorgiController>() == null) {
+        Debug.LogWarning("CameraController : The Player character doesn't have a CorgiController associated to it, the Camera won't work.");
+        return;
+      }
+
+      _targetController = target.GetComponent<CorgiController>();
     }
+  }
 }
--- SplitScreen/SplitScreenManager.cs
+++ SplitScreen/SplitScreenManager.cs
@@ -4,78 +4,102 @@ using UnityEngine;
 using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
 
-namespace CameraUtils
-{
-    /// <summary>
-    /// Calcule when player are two far away from each other and create a split screen.
-    /// When they get closer to each other, we go back to a full screen
-    /// </summary>
-    public class SplitScreenManager : MonoBehaviour
-    {
-        public enum CameraBehaviors { Single, SplitScreen };
-        [Header("Full Screen Camera")]
-        [Information("The Full Screen Camera is the camera that take all the screen.", MoreMountains.Tools.InformationAttribute.InformationType.Info, false)]
-        public Camera MainCamera;
-        [Header("Split Screen Camera")]
-        [Information("Set your left and right camera. Will display when player get a distance greater than the distance before splitting option.", MoreMountains.Tools.InformationAttribute.InformationType.Info, false)]
-        public Camera LeftCamera;
-        public Camera RightCamera;
-        [Header("Option")]
-        [Information("When camera should be splitted", MoreMountains.Tools.InformationAttribute.InformationType.Info, false)]
-        public float DistanceBeforeSplitting = 10f;
-        private CameraBehaviors _CameraBehavior = CameraBehaviors.Single;
-        private Transform _target1;
-        private Transform _target2;
-        private SplitScreen _splitScreenLeft;
-        private SplitScreen _splitScreenRight;
+namespace CameraUtils {
+  /// <summary>
+  /// SplitScreenManager toggle full screen to split screen when two
+  /// player are reach a certain distance between each other
+  /// When a player is leaving by the left, he's gonna be on the left camera
+  /// When he leaves by the right, he's gonna be on the right camera
+  ///
+  /// If you got any issues, you can contact create an issues on the github folder
+  /// https://github.com/Levrault/corgi-engine-dynamic-split-screen
+  ///
+  /// @author Levrault
+  /// </summary>
+  public class SplitScreenManager : MonoBehaviour {
+    // Camera states
+    public enum CameraBehaviors { Single, SplitScreen };
+    private CameraBehaviors _cameraBehavior = CameraBehaviors.Single;
 
-        // Use this for initialization
-        void Start()
-        {
-            _splitScreenLeft = LeftCamera.GetComponent<SplitScreen>();
-            _splitScreenRight = RightCamera.GetComponent<SplitScreen>();
-            _target1 = LevelManager.Instance.Players[0].transform;
-            _target2 = LevelManager.Instance.Players[1].transform;
-        }
+    [Header("Full Screen Camera")]
+    [Information("The Full Screen Camera is the main camera, the one that take all the screen.", MoreMountains.Tools.InformationAttribute.InformationType.Info, false)]
+    public Camera mainCamera;
 
-        void LateUpdate()
-        {
-            float distance = Vector3.Distance(_target1.localPosition, _target2.localPosition);
+    [Header("Split Screen Camera")]
+    [Information("Set the left/right camera. You will need to edit there viewport rec options to left them take half of the screen. Don't forgot to set your camera's Z offset value to -10. If you don't do it, you will have some issues on splitWarning screen mode.", MoreMountains.Tools.InformationAttribute.InformationType.Info, false)]
+    public Camera leftCamera;
+    public Camera rightCamera;
 
-            if (distance > DistanceBeforeSplitting)
-            {
-                _CameraBehavior = CameraBehaviors.SplitScreen;
-                // going left
-                if (_target1.localPosition.x < _target2.localPosition.x)
-                {
-                    _splitScreenLeft.SetTarget(_target1);
-                    _splitScreenRight.SetTarget(_target2);
-                }
-                else
-                { // going right
-                    _splitScreenLeft.SetTarget(_target2);
-                    _splitScreenRight.SetTarget(_target1);
-                }
+    [Header("Options")]
+    [Information("A trigger that will set show the splitted cameras instead of the full one when the distance between the two player is greater that this value ", MoreMountains.Tools.InformationAttribute.InformationType.Info, false)]
+    public float distanceBeforeSplitting = 10f;
 
-            }
-            else
-            {
-                _CameraBehavior = CameraBehaviors.Single;
-            }
+    // players positions
+    private Transform _target1;
+    private Transform _target2;
 
-            ToggleCamera();
-        }
+    // camera controller reference
+    private SplitScreen _splitScreenLeft;
+    private SplitScreen _splitScreenRight;
 
-        /// <summary>
-        /// Enable/disable camera depending on
-        /// the display mode
-        /// </summary>
-        void ToggleCamera()
-        {
-            bool isSplitScreen = (_CameraBehavior == CameraBehaviors.SplitScreen);
-            MainCamera.gameObject.SetActive(!isSplitScreen);
-            LeftCamera.gameObject.SetActive(isSplitScreen);
-            RightCamera.gameObject.SetActive(isSplitScreen);
-        }
+    private bool _isCameraNeedToBeTeleported = true;
+
+    void Start() {
+      // get left/right camera splitscreen component
+      _splitScreenLeft = leftCamera.GetComponent<SplitScreen>();
+      _splitScreenRight = rightCamera.GetComponent<SplitScreen>();
+
+      // get our two players
+      _target1 = LevelManager.Instance.Players[0].transform;
+      _target2 = LevelManager.Instance.Players[1].transform;
     }
+
+    void LateUpdate() {
+
+      // calculte the distance between the players
+      float distance = Vector3.Distance(_target1.localPosition, _target2.localPosition);
+
+      // if our distance is greater then our trigger, we split the camera
+      if (distance > distanceBeforeSplitting) {
+
+        // player is leaving by the left side, so he's gonna be on the left camera
+        if (_target1.localPosition.x < _target2.localPosition.x) {
+          _splitScreenLeft.SetTarget(_target1);
+          _splitScreenRight.SetTarget(_target2);
+        } else {
+          // player is leaving by the left side, so he's gonna be on the left camera
+          _splitScreenLeft.SetTarget(_target2);
+          _splitScreenRight.SetTarget(_target1);
+          // _splitScreenRight.TeleportCameraToTarget();
+        }
+
+        // if we don't teleport the camera, we will have a laggy camera movement each time
+        if (_isCameraNeedToBeTeleported) {
+          _splitScreenLeft.TeleportCameraToTarget();
+          _splitScreenRight.TeleportCameraToTarget();
+          _isCameraNeedToBeTeleported = false;
+        }
+
+        // switch camera
+        _cameraBehavior = CameraBehaviors.SplitScreen;
+        ToggleCamera();
+      } else if (distance <= distanceBeforeSplitting) {
+        // if our distance is lower then our trigger, we go full screen
+        _cameraBehavior = CameraBehaviors.Single;
+        // camera will need to be teleporter again when we will switch back to split screen
+        _isCameraNeedToBeTeleported = true;
+        ToggleCamera();
+      }
+    }
+
+    /// <summary>
+    /// Enable/disable camera depending on the script states
+    /// </summary>
+    void ToggleCamera() {
+      bool isSplitScreen = (_cameraBehavior == CameraBehaviors.SplitScreen);
+      mainCamera.gameObject.SetActive(!isSplitScreen);
+      leftCamera.gameObject.SetActive(isSplitScreen);
+      rightCamera.gameObject.SetActive(isSplitScreen);
+    }
+  }
 }
