882d2f3e056e655446b64b91d7455ae5b56ed5fd
Renaud Forestié <reuno@reuno.net>
2018-07-03 21:18:06 -0400

    :rocket: Adds 3 actions by Muppo

---
+++ ActionsAndDecisions/AIActionFlickering.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+
+	public class AIActionFlickering : AIAction
+	{
+		// Add this component to an AI Brain and agent speed will flicker the same way when damage is taken but enless.
+		// Muppo (2018)
+
+		public Color _flickerColor;
+
+		protected float _flickerSpeed = 0.05f; // Using higher values will work as a delay more than a real flicker
+		protected Renderer _renderer;
+		protected Character _character;
+		protected Color _initialColor = new Color32(255, 255, 255, 255);
+		protected float flickerDuration = 999999999f;
+
+		///
+	 	protected override void Initialization()
+        {
+			_renderer = GetComponent<SpriteRenderer>();
+
+
+			_character = GetComponent<Character>();
+			if (gameObject.GetComponentNoAlloc<SpriteRenderer>() != null)
+			{
+				_renderer = GetComponent<SpriteRenderer>();	
+				_initialColor = GetComponent<SpriteRenderer>().color;
+			}
+			if (_character != null)
+			{
+				if (_character.CharacterModel != null)
+				{
+					if (_character.CharacterModel.GetComponentInChildren<Renderer>() != null)
+					{
+						_renderer = _character.CharacterModel.GetComponentInChildren<Renderer>();	
+					}
+				}	
+			}
+		}
+
+		///
+		public override void PerformAction()
+		{
+			if (_renderer != null)
+			{
+				StartCoroutine(MMImage.Flicker(_renderer,_initialColor,_flickerColor,_flickerSpeed,flickerDuration));
+			}
+		}
+	}
+}
---
+++ ActionsAndDecisions/AIActionModifySpeed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+	// Add this component to an AI Brain and agent speed will be override with this value.
+	// Muppo (2018)
+
+	public class AIActionModifySpeed : AIAction
+	{
+		public float newSpeed;
+
+		protected CharacterHorizontalMovement _horizontalMovement;
+		protected float originalSpeed;
+
+
+	 	protected override void Initialization()
+        {
+            _horizontalMovement = this.gameObject.GetComponent<CharacterHorizontalMovement>();
+			originalSpeed = _horizontalMovement.WalkSpeed;
+        }
+		
+		public override void PerformAction()
+        {
+            _horizontalMovement.MovementSpeed = newSpeed;
+        }
+	}
+}
