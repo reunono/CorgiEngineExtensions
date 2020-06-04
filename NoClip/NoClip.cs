168057dd0bbcd2d4a3ad72e91ceb22a25b448723
Renaud Forestie <reuno@reuno.net>
2020-06-04 14:35:58 -0400

    ✨ Adds NoClip by Muppo

---
+++ NoClip/NoClip.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+    // Add this to your player prefab and it will have the classic NoClip ability.
+    public class NoClip : MonoBehaviour
+    {
+        [MMInformation("Toogle NoClip with <b>F4</b>",MoreMountains.Tools.MMInformationAttribute.InformationType.Info,false)]
+        [MMReadOnly] public bool noClip;
+     
+     
+        public virtual void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F4))
+            {
+                noClip = !noClip;
+            }
+
+            if(Input.GetKeyDown(KeyCode.F4) && !noClip)
+            {
+                Debug.Log("NOCLIP OFF");
+                GetComponent<CorgiController>().CollisionsOn();
+                GetComponent<CorgiController>().GravityActive(true);
+                GetComponent<Health>().Invulnerable = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.F4) && noClip)
+            {
+                Debug.Log("NOCLIP ON");
+                GetComponent<CorgiController>().CollisionsOff();
+                GetComponent<CorgiController>().GravityActive(false);
+                GetComponent<Health>().Invulnerable = true;
+            }
+        }
+    }
+}
