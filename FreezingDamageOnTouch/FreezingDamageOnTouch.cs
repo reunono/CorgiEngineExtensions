08bb9ae40d71271f1eca896f68345c8bca279805
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-06-27 12:46:03 +0300

    ✨ Adds FreezingDamageOnTouch

---
+++ FreezingDamageOnTouch/FreezingDamageOnTouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace CorgiEngineExtensions
+{
+    public class FreezingDamageOnTouch : DamageOnTouch
+    {
+        [Header("Freezing")]
+        [Tooltip("Whether or not to freeze on touch")]
+        public bool FreezeOnTouch = true;
+        [Tooltip("How long to freeze on touch")]
+        public float FreezeTime = 1;
+        protected override void Colliding(Collider2D collider)
+        {
+            base.Colliding(collider);
+            if (FreezeOnTouch) StartCoroutine(FreezeCollidingCharacter(collider));
+        }
+
+        protected IEnumerator FreezeCollidingCharacter(Collider2D collider)
+        {
+            var collidingCharacter = collider.gameObject.MMGetComponentNoAlloc<Character>();
+            collidingCharacter?.Freeze();
+            yield return new WaitForSeconds(FreezeTime);
+            collidingCharacter?.UnFreeze();
+        }
+    }
+}
