f875b182c4df0f04d01219453aacd95f2fe283de
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-10-12 23:43:40 +0300

    ✨ Adds CharacterDamageMultiplier

---
+++ CharacterDamageMultiplier/DamageMultiplierMeleeWeapon.cs
@@ -0,0 +1,9 @@
+﻿using MoreMountains.CorgiEngine;
+
+namespace CharacterDamageMultiplier
+{
+    public class DamageMultiplierMeleeWeapon : MeleeWeapon
+    {
+        public override void ApplyDamageMultiplier(float multiplier) { _damageOnTouch.DamageCaused = (int)(DamageCaused * multiplier); }
+    }
+}
---
+++ CharacterDamageMultiplier/PickableDamageMultiplier.cs
@@ -0,0 +1,24 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+namespace CharacterDamageMultiplier
+{
+    public class PickableDamageMultiplier : PickableItem
+    {
+        public int DamageMultiplier = 2;
+        private DamageMultiplierCharacterHandleWeapon _handleWeapon;
+
+        protected override void Pick()
+        {
+            _handleWeapon.DamageMultiplier = DamageMultiplier;
+        }
+
+        protected override bool CheckIfPickable()
+        {
+            _character = _collidingObject.MMGetComponentNoAlloc<Character>();
+            if (_character == null) return false;
+            _handleWeapon = _character.FindAbility<DamageMultiplierCharacterHandleWeapon>();
+            return _handleWeapon != null;
+        }
+    }
+}
