0b937206d36d9725e322598c408994f38c9b9d46
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-12-29 19:22:14 +0200

    ✨ Adds StatusEffectSystem

---
+++ StatusEffectSystem/Scripts/Others/ApplyStatusAndDamageOnTouch.cs
@@ -0,0 +1,60 @@
+﻿using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+namespace StatusSystem
+{
+    public class ApplyStatusAndDamageOnTouch : MultipliedDamageOnTouch
+    {
+        [Header("Status effects")]
+        [Tooltip("The list of status effects that will be applied to colliding characters (provided they have a CharacterStatusEffectListener with the required status effects)")]
+        public StatusEffect[] StatusEffects;
+
+        protected Character _character;
+
+        protected override void Colliding(Collider2D collider)
+        {
+            if (!this.isActiveAndEnabled)
+            {
+                return;
+            }
+
+            // if the object we're colliding with is part of our ignore list, we do nothing and exit
+            if (_ignoredGameObjects.Contains(collider.gameObject))
+            {
+                return;
+            }
+
+            // if what we're colliding with isn't part of the target layers, we do nothing and exit
+            if (!MMLayers.LayerInLayerMask(collider.gameObject.layer,TargetLayerMask))
+            {
+                return;
+            }
+
+            // we get the character and apply all the status effects in the list
+            _character = collider.gameObject.MMGetComponentNoAlloc<Character>();
+            if (_character != null)
+                foreach (var statusEffect in StatusEffects)
+                    StatusEffectEvent.Trigger(statusEffect, _character, StatusEffectEventTypes.Apply);
+            
+            _collidingCollider = collider;
+            _colliderHealth = collider.gameObject.MMGetComponentNoAlloc<Health>();
+
+            OnHit?.Invoke();
+
+            // if what we're colliding with is damageable
+            if ((_colliderHealth != null) && (_colliderHealth.enabled))
+            {
+                if(_colliderHealth.CurrentHealth > 0)
+                {
+                    OnCollideWithDamageable(_colliderHealth);
+                }
+            }
+            // if what we're colliding with can't be damaged
+            else
+            {
+                OnCollideWithNonDamageable();
+            }
+        }
+    }
+}
---
+++ StatusEffectSystem/Scripts/Others/MultipliedDamageOnTouch.cs
@@ -0,0 +1,48 @@
+﻿using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace StatusSystem
+{
+    public class MultipliedDamageOnTouch : DamageOnTouch
+    {
+        private CharacterDamageMultipliers _multipliers;
+        protected override void Awake()
+        {
+            base.Awake();
+            // the CharacterDamageMultipliers scriptable object must be in a Resources folder inside your project, like so : Resources/RuntimeSets/CharacterDamageMultipliers
+            _multipliers = Resources.Load<CharacterDamageMultipliers>("RuntimeSets/CharacterDamageMultipliers");
+        }
+
+        protected override void OnCollideWithDamageable(Health health)
+        {
+            // if what we're colliding with is a CorgiController, we apply a knockback force
+            _colliderCorgiController = health.gameObject.MMGetComponentNoAlloc<CorgiController>();
+
+            ApplyDamageCausedKnockback();
+			
+            OnHitDamageable?.Invoke();
+
+            HitDamageableFeedback?.PlayFeedbacks(this.transform.position);
+
+            if ((FreezeFramesOnHitDuration > 0) && (Time.timeScale > 0))
+            {
+                MMFreezeFrameEvent.Trigger(Mathf.Abs(FreezeFramesOnHitDuration));
+            }
+
+            // we apply the damage to the thing we've collided with
+            var character = Owner.MMGetComponentNoAlloc<Character>();
+            if (character != null)
+                _colliderHealth.Damage((int)(DamageCaused * _multipliers[character]), gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection);
+            else
+                _colliderHealth.Damage(DamageCaused, gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection);
+            if (_colliderHealth.CurrentHealth <= 0)
+            {
+                OnKill?.Invoke();
+            }
+			
+            SelfDamage(DamageTakenEveryTime + DamageTakenDamageable);
+        }
+    }
+}
