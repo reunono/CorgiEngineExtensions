35f711a466e0086c4dbe4d3d1718125eca5ca75f
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-04-07 14:58:56 +0300

    🚀 Adds PickableStamina

---
+++ StaminaExtension/Scripts/PickableStamina.cs
@@ -0,0 +1,24 @@
+﻿using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+namespace StaminaExtension
+{
+    /// <summary>
+    /// Gives stamina to the player who collects it
+    /// </summary>
+    [AddComponentMenu("Corgi Engine/Items/Pickable Stamina")]
+    public class PickableStamina : PickableItem
+    {
+        /// the amount of stamina to give the player when collected
+        [Tooltip("the amount of stamina to give the player when collected")]
+        public float StaminaToGive = 100;
+
+        /// <summary>
+        /// What happens when the object gets picked
+        /// </summary>
+        protected override void Pick()
+        {
+            _pickingCollider.GetComponent<Stamina>().CurrentStamina += StaminaToGive;
+        }
+    }
+}
