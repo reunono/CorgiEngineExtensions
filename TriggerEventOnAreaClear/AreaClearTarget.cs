ebc4fec20f3918fb8e275ad8228972e894f0f930
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-05-08 11:25:11 +0300

    ✨ Adds TriggerEventOnAreaClear

---
+++ TriggerEventOnAreaClear/AreaClearTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+namespace TriggerEventOnAreaClear
+{
+    /// <summary>
+    /// Add this class to an object and it will count as a target to clear (if it is inside a collider with a TriggerEventOnAreaClear component)
+    /// </summary>
+    public class AreaClearTarget : MonoBehaviour
+    {
+        private Health _health;
+        private TriggerEventOnAreaClear _triggerEventOnAreaClear;
+        private bool _alreadyRemoved;
+
+        private void Awake()
+        {
+            _health = GetComponent<Health>();
+            if (_health != null) _health.OnDeath += RemoveTarget;
+        }
+
+        private void AddTarget()
+        {
+            if (_triggerEventOnAreaClear == null) return;
+            _triggerEventOnAreaClear.AddTarget();
+            _alreadyRemoved = false;
+        }
+        
+        private void RemoveTarget()
+        {
+            if (_triggerEventOnAreaClear == null || _alreadyRemoved) return;
+            _triggerEventOnAreaClear.RemoveTarget();
+            _alreadyRemoved = true;
+        }
+        
+        private void OnEnable()
+        {
+            _triggerEventOnAreaClear = FindObjectsOfType<TriggerEventOnAreaClear>().SingleOrDefault(room => room.GetComponent<Collider2D>().bounds.Contains(transform.position));
+            AddTarget();
+        }
+
+        private void OnDisable() { RemoveTarget(); }
+    }
+}
---
+++ TriggerEventOnAreaClear/TriggerEventOnAreaClear.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TriggerEventOnAreaClear
+{
+    /// <summary>
+    /// Requires a Collider and objects with the AreaClearTarget component inside said collider. Make sure both this transform and the targets z positions are 0
+    /// </summary>
+    public class TriggerEventOnAreaClear : MonoBehaviour
+    {
+        [Tooltip("the event to trigger when the area is cleared (all objects marked with AreaClearTarget have been destroyed)")]
+        public UnityEvent OnAreaCleared;
+        private int _numberOfTargetsAlive;
+
+        public void AddTarget() { _numberOfTargetsAlive++; }
+
+        public void RemoveTarget()
+        {
+            _numberOfTargetsAlive--;
+            if (_numberOfTargetsAlive == 0) OnAreaCleared?.Invoke();
+        }
+    }
+}
